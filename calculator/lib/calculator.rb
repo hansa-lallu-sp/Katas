6ea99513fe527275814881767bbd0c4b7f8430a3
hansa-lallu-sp <hansa.lallu@smartpension.co.uk>
2021-03-05 22:17:00 +1300

    passing test

--- calculator/lib/calculator.rb
+++ calculator/lib/calculator.rb
@@ -1,6 +1,6 @@
 class Calculator
   def evaluate(string)
-    ['+', '*'].each do |operator|
+    ['+', '*', '-'].each do |operator|
         if string.include?(operator)
           return string.split(operator).map{ |x| evaluate(x) }.inject(operator)
         end
