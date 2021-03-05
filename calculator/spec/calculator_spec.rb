c5d5c9ca841949e38ea017ad2679724756f4d91f
hansa-lallu-sp <hansa.lallu@smartpension.co.uk>
2021-03-05 22:05:16 +1300

    refactored spec

--- calculator/spec/calculator_spec.rb
+++ calculator/spec/calculator_spec.rb
@@ -3,14 +3,12 @@ require 'calculator'
 describe Calculator do 
 
     it 'adds numbers when given a addition operator within the string' do 
-        calculator = Calculator.new
-        expect(calculator.evaluate('4 + 5')).to eq(9)
-        expect(calculator.evaluate('5 + 8')).to eq(13)
+        expect(subject.evaluate('4 + 5')).to eq(9)
+        expect(subject.evaluate('5 + 8')).to eq(13)
     end
 
     it 'multiples numbers when given a multiply operator within the string' do 
-        calculator = Calculator.new
-        expect(calculator.evaluate('4 * 5')).to eq(20)
+        expect(subject.evaluate('4 * 5')).to eq(20)
     end 
 
 end
