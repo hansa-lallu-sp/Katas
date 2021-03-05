e80499fa6e43a68839323de1cfcccec631a478e0
hansa-lallu-sp <hansa.lallu@smartpension.co.uk>
2021-03-05 21:41:47 +1300

    first passing test

--- calculator/spec/calculator_spec.rb
+++ calculator/spec/calculator_spec.rb
@@ -2,4 +2,9 @@ require 'calculator'
 
 describe Calculator do 
 
+    it 'adds numbers within a string' do 
+        calculator = Calculator.new
+        expect(calculator.evaluate("4 + 5")).to eq(9)
+    end 
+
 end
